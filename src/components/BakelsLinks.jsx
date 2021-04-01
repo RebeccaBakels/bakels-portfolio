716ec47d3aad7f3969341e6d7b4a575164bd81a5
Rebecca Bakels <rebeccabakels@gmail.com>
2021-04-01 17:38:44 -0400

    fixed css in buttons, updated LinkedIn

--- src/components/BakelsLinks.jsx
+++ src/components/BakelsLinks.jsx
@@ -6,26 +6,47 @@ function BakelsLinks() {
     <>
       <div className="button-group">
         <Button
-          style={{ backgroundColor: "#ffde59", marginTop: "10px" }}
+          style={{
+            backgroundColor: "#ffde59",
+            paddingLeft: "7vh",
+            paddingRight: "7vh",
+            marginTop: "10px",
+          }}
           size="large"
           variant="contained"
           href="https://github.com/RebeccaBakels"
+          target="_blank"
+          rel="noreferrer"
         >
           GitHub
         </Button>
         <Button
-          style={{ backgroundColor: "#ff9659", marginTop: "10px" }}
+          style={{
+            backgroundColor: "#ff9659",
+            paddingLeft: "7vh",
+            paddingRight: "7vh",
+            marginTop: "10px",
+          }}
           size="large"
           variant="contained"
-          href="https://www.linkedin.com/in/rebecca-bakels-311920139/"
+          href="https://www.linkedin.com/in/rebecca-bakels/"
+          target="_blank"
+          rel="noreferrer"
         >
           LinkedIn
         </Button>
         <Button
-          style={{ backgroundColor: "#ff6859", marginTop: "10px" }}
+          style={{
+            backgroundColor: "#ff6859",
+            paddingLeft: "7vh",
+            paddingRight: "7vh",
+            marginTop: "10px",
+          }}
           size="large"
           variant="contained"
           href="https://firebasestorage.googleapis.com/v0/b/bocacode-fb.appspot.com/o/candidates%2Fresumes%2FBakels_Resume_2021.pdf?alt=media"
+          target="_blank"
+          rel="noreferrer"
         >
           Resume
         </Button>
